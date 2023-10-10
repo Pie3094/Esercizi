cb77e7b99cacbaf25ce983db61517f33d920c5df
Pie3094 <145662095+Pie3094@users.noreply.github.com>
2023-10-10 12:31:11 +0200

    Carico Esercizio 2 modulo 3
    Esercizio con FORMA e Rettangolo

---
+++ forma/src/Forma.java
@@ -0,0 +1,5 @@
+public class Forma {
+    public void calcolaArea(int base, int altezza){
+        System.out.println("Il risultato della forma è : " + base * altezza);
+    }
+}
---
+++ forma/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Forma forma = new Forma();
+        Rettangolo rettangolo = new Rettangolo();
+        forma.calcolaArea(10, 5);
+        rettangolo.calcolaArea(4, 6);
+        }
+    }
+/* Scrivere un programma che contenga una classe chiamata Forma ed un metodo chiamato calcolaArea() che stampi l'area della forma.
+Crea poi una sottoclasse chiamata Rettangolo che fa override del metodo calcolaArea() per calcolare l'area del rettangolo. */
+
+// ctrl + shit + a softwrap
