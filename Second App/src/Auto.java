7147836f88cfdbe4597576a979afcd91a556d72a
Pie3094 <pietro.digio30@gmail.com>
2023-10-13 12:21:29 +0200

    Modifica correzione
    Cambiato il nome della classe Modello ==> Auto

---
+++ Second App/src/Auto.java
@@ -0,0 +1,58 @@
+public class Auto {
+    public String marca;
+
+    public String modello;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    @Override
+    public String toString() {
+        return "marca='" + marca + '\'' +
+                ", modello='" + modello + '\'' +
+                ", targa='" + targa + '\'' +
+                ", cilindrata=" + cilindrata +
+                '}';
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Auto(String marca, String modello, String targa, int cilindrata) {
+        this.marca = marca;
+        this.modello = modello;
+        this.targa = targa;
+        this.cilindrata = cilindrata;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+
+    public int getCilindrata() {
+        return cilindrata;
+    }
+
+    public void setCilindrata(int cilindrata) {
+        this.cilindrata = cilindrata;
+    }
+
+    public String targa;
+    public int cilindrata;
+
+}
+// cilindrata, targa, marca e modello.
--- Second App/src/Main.java
+++ Second App/src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         System.out.println("Inserisci il numero di auto: ");
         int selected = scan.nextInt();
 
-        Modelli macchina1 = new Modelli("Audi", "A1", "FB999AD", 1400);
-        Modelli macchina2 = new Modelli("Ford", "Fiesta", "Fb394EG", 1500);
-        Modelli macchina3 = new Modelli("Wolkswagen", "Golf", "JF359OL", 1900);
+        Auto macchina1 = new Auto("Audi", "A1", "FB999AD", 1400);
+        Auto macchina2 = new Auto("Ford", "Fiesta", "Fb394EG", 1500);
+        Auto macchina3 = new Auto("Wolkswagen", "Golf", "JF359OL", 1900);
         switch (selected) {
             case 1:
                 System.out.println("La macchina selezionata è: " + macchina1);
