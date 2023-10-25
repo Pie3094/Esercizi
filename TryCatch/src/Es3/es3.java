d03486c2fd057278c262b385a7317ac2cc8aef43
Pie3094 <145662095+Pie3094@users.noreply.github.com>
2023-10-25 13:39:50 +0200

    Es3
    es3

---
+++ TryCatch/src/Es3/es3.java
@@ -0,0 +1,17 @@
+package Es3;
+
+public class es3 {
+    public static void main(String[] args) throws Exception {
+        try {
+            divZero(10);
+        }catch (Exception e){
+            System.out.println("Non puoi dividere per zero, errore " + e);
+        }
+    }
+    public static void divZero(int x){
+        int result = x / 0;
+    }
+}
+
+
+//Scrivere una funzione che provi a dividere un numero per 0 e catturi l'eccezione leggendone il messaggio.
