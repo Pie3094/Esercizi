b9161de2bafc2d719898c40c6ffc3654c6ca8833
Pie3094 <145662095+Pie3094@users.noreply.github.com>
2023-10-26 11:15:43 +0200

    Carico esercizio null
    Null e nullability

---
+++ TryCatch/src/Nullability/Null.java
@@ -0,0 +1,39 @@
+package Nullability;
+
+public class Null {
+    public static void main(String[] args) {
+
+        System.out.println(nonNull(null, 0));
+
+    }
+
+    public static int nonNull(Integer numeratore, Integer denominatore) {
+        try {
+            if (numeratore != null && denominatore != null) {
+                if (denominatore != 0) {
+                    return numeratore / denominatore;
+                } else {
+                    throw new ArithmeticException("Non posso dividere per zero");
+                }
+            } else {
+                throw new NullPointerException("Uno dei due valori è null, errore.");
+            }
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+//              userConsent == null;
+//          try {
+//            if (userConsent == true) {
+//                System.out.println("True!");
+//            } else {
+//                System.out.println("Nope!");
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println("Nessun valore" + e);
+//        }
+
+}
